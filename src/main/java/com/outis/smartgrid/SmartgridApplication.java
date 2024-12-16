e7884bfa9b30f4b24185487e2bc5c78a85b14b54
Narciso Parra Duarte <outis10@gmail.com>
2024-12-15 18:25:22 -0700

    Update README.md

---
+++ src/main/java/com/outis/smartgrid/SmartgridApplication.java
@@ -0,0 +1,13 @@
+package com.outis.smartgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgridApplication.class, args);
+	}
+
+}
